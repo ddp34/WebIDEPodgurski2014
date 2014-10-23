225d95d8faef91451bbdce0b307bbe8a14c594cb
darian <hegemonofdarkness@gmail.com>
2014-10-23 19:40:33 -0400

    custom html for login page

--- webide/web_ide/views.py
+++ webide/web_ide/views.py
@@ -1,9 +1,8 @@
-from django.template import render
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return HttpResponse("login page")
+    return render(request, 'web_ide/login.html')
 
 def admin(request):
     return HttpResponse("admin page")
