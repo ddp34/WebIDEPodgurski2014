03fedace9cffc50838b59b41a94324073f1d1c57
g-ewing <gre5@case.edu>
2014-10-22 21:26:14 -0400

    added httpresponses to views

--- webide/web_ide/views.py
+++ webide/web_ide/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def login(request):
+    return HttpResponse("login page")
+
+def admin(request):
+    return HttpResponse("admin page")
+
+def editor(request):
+    return HttpResponse("editor page")
