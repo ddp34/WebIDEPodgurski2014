18281f8210f01258750db2d2697f7ed99ac43db3
darian <hegemonofdarkness@gmail.com>
2014-10-29 19:47:00 -0400

    added basic text editing area with tweaks, started to add model for filesystem representation

--- webide/web_ide/models.py
+++ webide/web_ide/models.py
@@ -9,3 +9,17 @@ class UserProfile(models.Model):
 
     def __unicode__(self):
         return self.user.username
+
+#class to represent the filesystem
+class FileSystem(models.Model):
+    filename = models.CharField(max_length=64)
+    contents = models.CharField(max_length=2056)
+
+    def getName(self):
+        return self.name
+
+    def getContents(self):
+        return self.contents
+
+    def isDir(self, path):
+        return os.path.isdir(path)
