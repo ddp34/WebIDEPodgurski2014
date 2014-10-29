9f8c32ebefd9ac70e2ada3acee105099c864a74c
darian <hegemonofdarkness@gmail.com>
2014-10-29 11:36:14 -0400

    map login page to project root

--- webide/web_ide/urls.py
+++ webide/web_ide/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from web_ide import views
 
 urlpatterns = patterns('',
-                       url(r'login', views.login, name='login'),
+                       url(r'^$', views.login, name='login'),
                        url(r'admin', views.admin, name='admin'),
                        url(r'editor', views.editor, name='editor'))
