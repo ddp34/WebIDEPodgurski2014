9bfe7c80f48a1a026b293205f5fe986f55ecd01e
g-ewing <gre5@case.edu>
2014-10-28 20:53:09 -0400

    Added user system, made necessary html edits for login and register pages, added logout ability, access restriction

--- webide/web_ide/urls.py
+++ webide/web_ide/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, url
 from web_ide import views
 
 urlpatterns = patterns('',
-                       url(r'login', views.login, name='login'),
+                       url(r'login', views.user_login, name='login'),
                        url(r'admin', views.admin, name='admin'),
-                       url(r'editor', views.editor, name='editor'))
+                       url(r'register', views.register, name='register'),
+                       url(r'editor', views.editor, name='editor'),
+                       url(r'restricted', views.restricted, name='restricted'),
+                       url(r'logout', views.user_logout, name='logout'))
--- webide/web_ide/views.py
+++ webide/web_ide/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from web_ide.forms import UserForm
 
-def login(request):
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/web_ide/')
+            else:
+                return HttpResponse("Inactive account.")
+        else:
+            print "Login details invalid: {0}, {1}".format(username, password)
+            return HttpResponse("Login details invalid")
+    else:
+        return render(request, 'web_ide/login.html', {})
+
     return render(request, 'web_ide/login.html')
 
 def admin(request):
@@ -9,3 +30,36 @@ def admin(request):
 
 def editor(request):
     return render(request, 'web_ide/editor.html')
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            registered = True
+
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form = UserForm()
+
+    return render(request, 'web_ide/register.html',
+                  {'user_form': user_form, 'registered': registered})
+
+@login_required
+def restricted(request):
+    return HttpResponse("You are logged in.")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect
