5f3b5ac11b7b2815b28b529a626c905baf392928
darian <hegemonofdarkness@gmail.com>
2014-10-29 23:41:52 -0400

    resolved merge conflicts in editor template; added first unit test for username validation

--- webide/web_ide/tests.py
+++ webide/web_ide/tests.py
@@ -1,7 +1,24 @@
 from django.test import TestCase
 from web_ide.models import DeveloperManager
 from web_ide.models import Developer
-from web_ide.models import FileSystem
+from web_ide.forms import DeveloperForm
+#from web_ide.models import FileSystem
+
+#test regexes for user credentials
+class UserCredentialsTestCase(TestCase):
+
+    #test username validation
+    def test_username_validation(self):
+        
+        #valid fields
+        user_form_valid = DeveloperForm({'username': "testusername", 'email': "email@abc.com", 'password': "password"})
+        self.assertEqual(user_form_valid.is_valid(), True)
+
+        #username longer than 30 characters, should be false
+        user_form_too_long = DeveloperForm({'username': "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 'email': "email@abc.com", 'password': "password"})
+        self.assertEqual(user_form_too_long.is_valid(), False)
+
+    #TODO: test password/email validation
 
 # Django's example:
 '''
@@ -31,13 +48,3 @@ $ ./manage.py test animals.tests.AnimalTestCase
 # Run just one test method
 $ ./manage.py test animals.tests.AnimalTestCase.test_animals_can_speak
 '''    
- 
- 
-# Test the DeveloperManager model
-class DeveloperManagerTestCase(TestCase):    
-    
-# Test the Developer model
-class DeveloperTestCase(TestCase):
-
-# Test the FileSystem model
-class FileSystemTestCase(TestCase):
