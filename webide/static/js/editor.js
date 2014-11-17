4f2bdd7e9683804593701e51540d96812f2415e6
darian <hegemonofdarkness@gmail.com>
2014-11-16 20:04:14 -0500

    keep tab key from switching input fields in editor view

--- webide/static/js/editor.js
+++ webide/static/js/editor.js
@@ -1,9 +1,37 @@
+//this code was partially borrowed from online: http://stackoverflow.com/questions/946534/insert-text-into-textarea-with-jquery/946556#946556
+//we didnt invent this insertion function
+$.fn.extend({
+	insertTab: function(text) {
+	    var sel, range, html;
+	    if (window.getSelection) {
+		sel = window.getSelection();
+		if(sel.getRangeAt && sel.rangeCount) {
+		    //range = sel.getRangeAt(0);
+		    //range.deleteContents();
+		    //var indent = document.createElement("p");
+		    //indent.style.textIndent = "4em";
+		    //range.surroundContents(indent);
+		}
+	    } else if (document.selection && document.selection.createRange) {
+		document.selection.createRange().text = text;
+	    }
+	}
+    })
+
+//This is our code
 $(document).ready(function() {
-	//set listener
+	//set key listener
 	$(".filecontentsview").keydown(function(e) {
+		//override 'tab' key for indenting source code
 		if(e.keyCode==9) {
-		    e.currentTarget.insert("\t");
-		    e.stop();
+		    e.preventDefault();
+		    //indent the line by 4 spaces
+		    $(".filecontentsview").insertTab("");
+		}
+	    });
+	$(".filecontentsview").keyup(function(e) {
+		if(e.keyCode==9) {
+		    $(".filecontentsview").focus();
 		}
 	    });
 });
