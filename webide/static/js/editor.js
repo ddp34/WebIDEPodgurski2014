37a2d2509ec88372e6c1c38cd69762d3ef7391fb
darian <hegemonofdarkness@gmail.com>
2014-11-24 21:01:50 -0500

    added ability to tab source code in editor

--- webide/static/js/editor.js
+++ webide/static/js/editor.js
@@ -1,33 +1,47 @@
-//this code was partially borrowed from online: http://stackoverflow.com/questions/946534/insert-text-into-textarea-with-jquery/946556#946556
-//we didnt invent this insertion function
-$.fn.extend({
-	insertTab: function(text) {
-	    var sel, range, html;
-	    if (window.getSelection) {
-		sel = window.getSelection();
-		if(sel.getRangeAt && sel.rangeCount) {
-		    //don't do anything until we figure out how to insert a tab at the caret
-		    //range = sel.getRangeAt(0);
-		    //range.deleteContents();
-		    //var indent = document.createElement("p");
-		    //indent.style.textIndent = "4em";
-		    //range.surroundContents(indent);
-		}
-	    } else if (document.selection && document.selection.createRange) {
-		document.selection.createRange().text = text;
+//Borrowed from Stack Overflow to insert text into a position in an HTML textarea:
+//http://stackoverflow.com/questions/1064089/inserting-a-text-where-cursor-is-using-javascript-jquery
+function insertAtCaret(areaId, text) {
+	    var txtarea = document.getElementById(areaId);
+	    var scrollPos = txtarea.scrollTop;
+	    var strPos = 0;
+	    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? 
+		      "ff" : (document.selection ? "ie" : false ) );
+	    if (br == "ie") { 
+		txtarea.focus();
+		var range = document.selection.createRange();
+		range.moveStart ('character', -txtarea.value.length);
+		strPos = range.text.length;
 	    }
-	}
-    })
+	    else if (br == "ff") strPos = txtarea.selectionStart;
 
-//This is our code
+	    var front = (txtarea.value).substring(0,strPos);  
+	    var back = (txtarea.value).substring(strPos,txtarea.value.length); 
+	    txtarea.value=front+text+back;
+	    strPos = strPos + text.length;
+	    if (br == "ie") { 
+		txtarea.focus();
+		var range = document.selection.createRange();
+		range.moveStart ('character', -txtarea.value.length);
+		range.moveStart ('character', strPos);
+		range.moveEnd ('character', 0);
+		range.select();
+	    }
+	    else if (br == "ff") {
+		txtarea.selectionStart = strPos;
+		txtarea.selectionEnd = strPos;
+		txtarea.focus();
+	    }
+	    txtarea.scrollTop = scrollPos;
+}
+
+//code to be called immediately when page is loaded
 $(document).ready(function() {
 	//set key listener
 	$(".filecontentsview").keydown(function(e) {
 		//override 'tab' key for indenting source code
 		if(e.keyCode==9) {
 		    e.preventDefault();
-		    //indent the line by 4 spaces
-		    $(".filecontentsview").insertTab("");
+		    insertAtCaret("fileview", "\t");
 		}
 	    });
 	$(".filecontentsview").keyup(function(e) {
@@ -35,7 +49,4 @@ $(document).ready(function() {
 		    $(".filecontentsview").focus();
 		}
 	    });
-
-	//begin polling for updates
-	
 });
