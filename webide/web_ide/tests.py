0960693bb28e256a348a4c7b9f10dd5db7a33fa0
darian <hegemonofdarkness@gmail.com>
2014-12-08 19:07:09 -0500

    blank slate for tests

--- webide/web_ide/tests.py
+++ webide/web_ide/tests.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
-from web_ide.models import DeveloperManager
-from web_ide.models import Developer
-from web_ide.models import ProjectFile
-from web_ide.forms import DeveloperForm
-from webide.web_ide.syntax.highlighting_engine import HighlightingEngine
-from webide.web_ide.syntax.plyj.ply.lex import LexToken
+from web_ide.models import *
+from web_ide.forms import CustomDeveloperCreationForm
+
+from diffsync import DiffSync
+
+'''
 #test regexes for user credentials
 class UserCredentialsTestCase(TestCase):
 
@@ -12,29 +12,29 @@ class UserCredentialsTestCase(TestCase):
     def test_username_validation(self):
         
         #valid fields
-        user_form_valid = DeveloperForm({'username': "testusername", 'email': "email@abc.com", 'password': "password"})
+        user_form_valid = CustomDeveloperCreationForm({'username': "testusername", 'email': "email@abc.com", 'password': "password"})
         self.assertEqual(user_form_valid.is_valid(), True)
 
         #username longer than 30 characters, should be false
-        user_form_too_long = DeveloperForm({'username': "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 'email': "email@abc.com", 'password': "password"})
+        user_form_too_long = CustomDeveloperCreationForm({'username': "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 'email': "email@abc.com", 'password': "password"})
         self.assertEqual(user_form_too_long.is_valid(), False)
 
     #test email validation
     def test_email_validation(self):
         
         #valid fields
-        user_form_valid = DeveloperForm({'username': "testusername", 'email': "email@abc.com", 'password': "password"})
+        user_form_valid = CustomDeveloperCreationForm({'username': "testusername", 'email': "email@abc.com", 'password': "password"})
         self.assertEqual(user_form_valid.is_valid(), True)
 
         #email is invalid
-        user_form_bad_email = DeveloperForm({'username': "testusername", 'email': "bademail", 'password': "password"})
+        user_form_bad_email = CustomDeveloperCreationForm({'username': "testusername", 'email': "bademail", 'password': "password"})
         self.assertEqual(user_form_bad_email.is_valid(), False)
 
 #tests for model 
 class DeveloperTestCase(TestCase):
     def test_getter_methods(self):
         #create developer object
-        user_form_valid = DeveloperForm({'username': "testusername", 'email': "email@abc.com", 'password': "password"})
+        user_form_valid = CustomDeveloperCreationForm({'username': "testusername", 'email': "email@abc.com", 'password': "password"})
         user = user_form_valid.save()
         user.set_password(user.password)
         
@@ -42,22 +42,4 @@ class DeveloperTestCase(TestCase):
         self.assertEqual(user.get_full_name(), "testusername")
         self.assertEqual(user.get_short_name(), "testusername")
 
-class SyntaxTestCase(TestCase):
-    # tests for highlighting single element
-    ''' def highlight_element(self, token):
-        type = token.type
-        for k, v in self.color_table.iteritems():
-            if (type == k): return v
-            else: return 'default' '''
-
-    def test_token_mapping(self):
-        he = HighlightingEngine()
-        hard_mapping = he.color_table["INT"]
-
-        test_tok = LexToken('INT', 'int', 1, 1)
-        test_mapping = he.highlight_element(test_tok)
-
-        self.assertEqual(hard_mapping, test_mapping)
-
-
-
+'''
