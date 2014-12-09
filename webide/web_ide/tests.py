9951b18fc3a572a335d5aecfd1354e07ab7df72e
darian <hegemonofdarkness@gmail.com>
2014-12-08 19:25:00 -0500

    start writing diffsync tests

--- webide/web_ide/tests.py
+++ webide/web_ide/tests.py
@@ -43,3 +43,46 @@ class DeveloperTestCase(TestCase):
         self.assertEqual(user.get_short_name(), "testusername")
 
 '''
+
+'''
+Tests the differential synchronization engine,
+written in diffsync.py
+'''
+class DiffSyncTestCase(TestCase):
+    
+    def setUp(self):
+
+        #server text
+        ServerText.objects.create(filename="test.txt", text="The quick brown fox jumps over the lazy dog.")
+
+        #user a's server shadow
+        ServerShadow.objects.create(filename="test.txt", text="The quick brown fox jumps over the lazy dog.", name="a")
+
+        #user b's server shadow
+        ServerShadow.objects.create(filename="test.txt", text="The quick brown fox jumps over the lazy dog.", name="b")
+        
+    #tests text sync for two users across a single file
+    def test_single_file_sync(self):
+
+        diff_sync = DiffSync()
+
+        #user a variables
+        a_clienttext = "The quick red fox jumps over the lazy dog."
+        a_clientshadow = "The quick brown fox jumps over the lazy dog."
+        a_servershadow = ServerShadow.objects.get(name="a")
+
+        #user b variables
+        b_clienttext = "The quick brown fox jumps above the lazy dog."
+        b_clientshadow = "The quick brown fox jumps over the lazy dog."
+        b_servershadow = ServerShadow.objects.get(name="b")
+
+        #grab objects
+        servertext = ServerText.objects.get(filename="test.txt")
+
+        #suppose user a syncs their work, and then user b
+        sync_results_1 = diff_sync.synchronizeDocs(a_clienttext, a_clientshadow, a_servershadow.text, servertext.text)
+
+        #check that the resulting client text and shadow for a are valid
+        self.assertEqual(sync_results_1[0], "The quick red fox jumps over the lazy dog.")
+
+        self.assertEqual("", "")
